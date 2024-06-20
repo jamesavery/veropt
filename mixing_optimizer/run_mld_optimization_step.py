a33597406b5780e50f566f2fa183d1e7cd2ef012
James Avery <avery@ece.au.dk>
2024-06-20 15:37:23 +0200

    MLD-optimization run script for 1deg MLD-experiment on Threadripper

--- mixing_optimizer/run_mld_optimization_step.py
+++ mixing_optimizer/run_mld_optimization_step.py
@@ -1,13 +1,29 @@
 from veropt import BayesOptimiser
-from veropt.obj_funcs.mld_objective_function import *
+from veropt.obj_funcs.mld_objective_function_2d_parallel import *
 from veropt.gui import veropt_gui
+from veropt.experiment import *
+import datetime as dt
 
-n_init_points = 1
-n_bayes_points = 1
-n_evals_per_step = 1
+n_init_points = 10
+n_bayes_points = 30
+n_evals_per_step = 2
+experiment = "mld_experiment7"
+# criterion = 'rho'
 
-obj_func = MLD1ObjFun()
+params = ["c_k", "c_eps"]
+param_list = f'/data/ocean/veropt_results/{experiment}/param_list_{experiment}.txt'
+
+with open(param_list, "w") as file:
+    file.write(f"{params[0]},{params[1]},obj_func")
+
+now = dt.datetime.now()
+
+obj_func = MLD1ObjFun(param_list)
 
 optimiser = BayesOptimiser(n_init_points, n_bayes_points, obj_func, n_evals_per_step=n_evals_per_step)
 
-veropt_gui.run(optimiser)
+# veropt_gui.run(optimiser)
+
+optimiser.run_all_opt_steps()
+
+optimiser.save_optimiser(f'nbayes_{n_bayes_points}_{experiment}_{now.day}-{now.month}-{now.year}_1deg_2d_parallel_opt.pkl')
