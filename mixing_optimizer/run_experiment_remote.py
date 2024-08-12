644d33642146fe9a440ac45c2acfa0c6ebecfb0d
James Avery <avery@nbi.dk>
2024-08-12 18:12:16 +0200

    Made beta and lengthscale optional in optimizer.json. Added random_seed option for reproducibility.

--- mixing_optimizer/run_experiment_remote.py
+++ mixing_optimizer/run_experiment_remote.py
@@ -3,15 +3,13 @@ from veropt.obj_funcs.mld_obj_function_remoteslurm import *
 from veropt.experiment import *
 import sys, pyjson5 as json, datetime as dt
 
-# Call as: pytho3 run_mld_remote.py <experiment_name> <remote_server_name>
-# E.g.: python3 run_mld_remote.py 4deg_tke_eke_wind_observations lumi
+# Call as: pytho3 run_experiment_remote.py <experiment_name> <remote_server_name>
+# E.g.: python3 run_experiment_remote.py 4deg_tke_eke_wind_observations lumi
 try: 
     experiment_name, remote_server_name = sys.argv[1:3]
 except:
-    print(f"Syntax: python3 run_mld_remote.py <experiment_name> <remote_server_name> [optimizer_config_name='optimizer']")
+    print(f"Syntax: python3 run_mld_remote.py <experiment_name> <remote_server_name>")
     sys.exit(1)
-if(len(sys.argv) > 3):
-    optimizer_config_name = sys.argv[3]
 
 local_cfg = {
  'datadir':  "/data/ocean/",
@@ -60,10 +58,10 @@ else:
 
 # READ IN OPTIMIZER CONFIG
 try: 
-    with open(f"{experiment_dir}/{optimizer_config_name}.json","r") as f:
+    with open(f"{experiment_dir}/optimizer.json","r") as f:
         opt_cfg = json.load(f)
 except Exception as e: 
-    print(f"While reading {experiment_dir}/{optimizer_config_name}.json:\n\t{e}")
+    print(f"While reading {experiment_dir}/optimizer.json:\n\t{e}")
     sys.exit(4)    
 
 
@@ -79,9 +77,15 @@ n_init_rounds    = opt_cfg['n_init_rounds']
 n_bayes_rounds   = opt_cfg['n_bayes_rounds']
 n_evals_per_step = opt_cfg['n_evals_per_step']
 
-optimiser = BayesOptimiser(n_init_rounds*n_evals_per_step, n_bayes_rounds*n_evals_per_step, obj_func, n_evals_per_step=n_evals_per_step)
-optimiser.model.constraint_dict_list[0]["covar_module"]["raw_lengthscale"] = opt_cfg['raw_lengthscale']
-optimiser.set_acq_func_params("beta", opt_cfg['beta'])
+if "random_seed" in opt_cfg:
+    random_seed      = opt_cfg['random_seed']
+    print(f"Reproducible run: random_seed={random_seed}")
+else:
+    random_seed = None
+
+optimiser = BayesOptimiser(n_init_rounds*n_evals_per_step, n_bayes_rounds*n_evals_per_step, obj_func, n_evals_per_step=n_evals_per_step, random_seed=random_seed)
+if 'raw_lengthscale' in opt_cfg: optimiser.model.constraint_dict_list[0]["covar_module"]["raw_lengthscale"] = opt_cfg['raw_lengthscale']
+if 'beta'            in opt_cfg: optimiser.set_acq_func_params("beta", opt_cfg['beta'])
 
 # optimiser.run_all_opt_steps()
 
